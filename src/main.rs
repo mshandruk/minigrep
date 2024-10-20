603bda378feded1f4271898322b65cc4a308731b
Maxim Shandruk <mxshandruk@gmail.com>
2024-10-20 21:55:53 +0300

    Add business logic of application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, process, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let content = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("With text content:\n{}", content);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
