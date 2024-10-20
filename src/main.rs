6a76a907184214dba83a5722ea86de5efa5629f7
Maxim Shandruk <mxshandruk@gmail.com>
2024-10-20 22:32:52 +0300

    Splitting Code into a Library Crate, move business logic from main to lib

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,20 @@
-use std::{env, process, fs, error::Error};
+use std::{env, process};
+
+use minigrep::{Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
     println!("Searching for {}", config.query);
-    if let Err(e) = run(config) {
-        println!("Application error: {e}");
+    println!("In file {}", config.file_path);
+
+    if let Err(error) = minigrep::run(config) {
+        println!("Application error: {error}");
         process::exit(1);
-    }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
     }
 }
