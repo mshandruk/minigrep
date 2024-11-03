fb93523aa87ade1760d249de552e5317b534ac6a
Maxim Shandruk <mxshandruk@gmail.com>
2024-11-03 13:57:13 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -40,23 +40,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
+    let query = query.to_lowercase();
+
     for line in contents.lines() {
-        if line.contains(query) {
+        if line.to_lowercase().contains(&query) {
             results.push(line)
         }
     }
     results
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-
     for line in contents.lines() {
-        if line.to_lowercase().contains(&query)
-        {
+        if line.contains(query) {
             results.push(line)
         }
     }
@@ -86,10 +85,9 @@ safe, fast, productive.
 Pick three.
 Trust me.
 ";
-
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
-        )
+        );
     }
 }
